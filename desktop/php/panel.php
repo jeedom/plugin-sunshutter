56590ac44c09dce419c93aa79c5a0548557464de
Loïc <loic@jeedom.com>
2019-06-16 13:32:57 +0200

    typo

--- desktop/php/panel.php
+++ desktop/php/panel.php
@@ -4,38 +4,39 @@ if (!isConnect()) {
 }
 ?>
 <div>
-<br/>
-<form class="form-horizontal">
-	<fieldset>
-		<div class="form-group">
-			<label class="col-sm-2 control-label">{{Position Moyenne}}</label>
-			<div class="col-sm-1">
-				<span class="label label-success posMoy"></span>
+	<br/>
+	<form class="form-horizontal">
+		<fieldset>
+			<div class="form-group">
+				<label class="col-sm-2 control-label">{{Position Moyenne}}</label>
+				<div class="col-sm-1">
+					<span class="label label-success posMoy"></span>
+				</div>
+				<label class="col-sm-2 control-label">{{Suspendu Manuel}}</label>
+				<div class="col-sm-1">
+					<span class="label label-danger manualSuspend"></span>
+				</div>
+				<label class="col-sm-2 control-label">{{Suspendu Auto}}</label>
+				<div class="col-sm-1">
+					<span class="label label-warning autoSuspend"></span>
+				</div>
 			</div>
-			<label class="col-sm-2 control-label">{{Suspendu Manuel}}</label>
-			<div class="col-sm-1">
-				<span class="label label-danger manualSuspend"></span>
-			</div>
-			<label class="col-sm-2 control-label">{{Suspendu Auto}}</label>
-			<div class="col-sm-1">
-				<span class="label label-warning autoSuspend"></span>
-			</div>
-		</div>
-	</fieldset>
+		</fieldset>
 	</form>
 </div>
 <table class="table table-condensed tablesorter" id="table_sunshutter">
 	<thead>
 		<tr>
-            <th data-priority="1">{{Nom}}</th>
+			<th data-priority="1">{{Nom}}</th>
 			<th data-priority="1">{{Azimuth}}</th>
 			<th data-priority="1">{{Elevation}}</th>
 			<th data-priority="1">{{Dernier}}</th>
 			<th data-priority="1">{{Suspension}}</th>
-            <th data-priority="1">{{Gestion}}</th>
-            <th data-priority="1">{{Manuel}}</th>
+			<th data-priority="1">{{Gestion}}</th>
+			<th data-priority="1">{{Manuel}}</th>
 		</tr>
 	</thead>
 	<tbody>
-<table>
+	</tbody>
+</table>
 <?php include_file('desktop', 'panel', 'js', 'sunshutter');?>
