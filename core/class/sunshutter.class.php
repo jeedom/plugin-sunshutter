146a427dd5f551896c0be959aa0bf0f684758df6
loic <loic@jeedom.com>
2021-01-14 09:00:56 +0100

    improvements

--- core/class/sunshutter.class.php
+++ core/class/sunshutter.class.php
@@ -348,6 +348,8 @@ public function postSave() {
     }
     if ($nblistener > 0) {
       $listener->save();
+    }else{
+      $listener->remove();
     }
   } else {
     $listener = listener::byClassAndFunction('sunshutter', 'immediateAction', array('sunshutter_id' => intval($this->getId())));
