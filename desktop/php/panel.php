52b23259d79da96a96e267411cad95234d3a0187
sarakha63 <ludovic@jeedom.com>
2022-11-25 15:20:30 +0100

    Update panel.php

--- desktop/php/panel.php
+++ desktop/php/panel.php
@@ -7,9 +7,9 @@ sendVarToJs('jeedomUtils.backgroundIMG', 'plugins/sunshutter/core/img/panel.jpg'
 <div>
 	<br />
 	<legend><center>
-		{{Position Moyenne}} <span class="label label-success posMoy"></span>
-		{{Suspendu Manuel}} <span class="label label-danger manualSuspend"></span>
-		{{Suspendu Auto}} <span class="label label-warning autoSuspend"></span>
+		{{Position Moyenne}} <i class="icon jeedom-volet-ouvert"></i> <span class="label label-success posMoy"></span>
+		{{Suspendu Manuel}} <i class="fas fa-hand-paper"></i> <span class="label label-danger manualSuspend"></span>
+		{{Suspendu Auto}} <i class="fas fa-robot"></i> <span class="label label-warning autoSuspend"></span>
 	</center></legend>
 </div>
 <div class="div_displayEquipement" style="width: 100%;">
